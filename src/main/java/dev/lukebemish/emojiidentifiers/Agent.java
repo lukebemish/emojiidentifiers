40e399c78f62a8af741b4411520feace371cca14
Luke Bemish <lukebemish@lukebemish.dev>
2024-04-10 20:11:12 -0500

    Remove needless check

--- src/main/java/dev/lukebemish/emojiidentifiers/Agent.java
+++ src/main/java/dev/lukebemish/emojiidentifiers/Agent.java
@@ -9,11 +9,6 @@ public final class Agent {
     public static void premain(String agentArgs, Instrumentation instrumentation) throws UnmodifiableClassException {
         instrumentation.addTransformer(new CharacterTransformer(), true);
         instrumentation.retransformClasses(Character.class);
-        try {
-            Class.forName("dev.lukebemish.emojiidentifiers.EmojiChecker", true, ClassLoader.getSystemClassLoader());
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
         System.out.println("emojiidentifiers agent loaded; weird behavior may ensue");
     }
 }
